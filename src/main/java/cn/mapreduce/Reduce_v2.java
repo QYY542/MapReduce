2d177a41c1015e84c4f8ce299e010715dbbf3bca
QYY542 <2460603567@qq.com>
2022-09-29 03:57:35 +0800

    finish_03

--- src/main/java/cn/mapreduce/Reduce_v2.java
+++ src/main/java/cn/mapreduce/Reduce_v2.java
@@ -12,7 +12,6 @@ import java.nio.charset.StandardCharsets;
 public class Reduce_v2 extends TableReducer<Text, Text, ImmutableBytesWritable> {
     private static StringBuilder sub = new StringBuilder();
     private static Text word = new Text();
-    private static Text index = new Text();
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context)
@@ -24,18 +23,17 @@ public class Reduce_v2 extends TableReducer<Text, Text, ImmutableBytesWritable> 
             file_count += 1;
             String[] splits = value.toString().split(":");
             total_count += Long.parseLong(splits[1]);
-            sub.append(value).append(";");
+            sub.append(value.toString()).append(";");
         }
         word.set(key);
-        index.set(sub.toString());
         //<word,(fileName_1:count_1:position_1);(fileName_2:count_2:position_2)>
 //        context.write(word, index);
         Put put = new Put(key.toString().getBytes());
-        put.addColumn("items".getBytes(), "item".getBytes(), index.getBytes());
+        put.addColumn("items".getBytes(), "item".getBytes(), sub.toString().getBytes());
         put.addColumn("counts".getBytes(), "file_count".getBytes(), String.valueOf(file_count).getBytes());
         put.addColumn("counts".getBytes(), "total_count".getBytes(), String.valueOf(total_count).getBytes());
         sub.delete(0, sub.length());
-        context.write(new ImmutableBytesWritable(Bytes.toBytes(key.toString())), put);
+        context.write(null, put);
 
     }
 }
